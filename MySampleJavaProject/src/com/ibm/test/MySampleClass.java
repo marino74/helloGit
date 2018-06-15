14c1b3133eb12b5b0b06499cf4400e14da67ad6c
marino74 <marino.aguilera.sola@gmail.com>
2018-06-15 10:02:47 -0500

    Primer comentario commit

---
+++ MySampleJavaProject/src/com/ibm/test/MySampleClass.java
@@ -0,0 +1,15 @@
+package com.ibm.test;
+
+public class MySampleClass {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
